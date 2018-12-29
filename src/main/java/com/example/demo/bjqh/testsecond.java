d8647dba52f4af26883f553aa4c42fe8d1ea23be
WestRedEyes <345234710@qq.com>
2018-12-29 11:02:24 +0800

    third commit

--- src/main/java/com/example/demo/bjqh/testsecond.java
+++ src/main/java/com/example/demo/bjqh/testsecond.java
@@ -4,4 +4,7 @@ package com.example.demo.bjqh;
  * Created by liwenhao on 2018/12/29.
  */
 public class testsecond {
+    public static void main(String[] args) {
+        System.out.println("有变更");
+    }
 }
